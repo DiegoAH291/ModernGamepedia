7472097aa88122fc63caf949efaf7ac5b3904d6d
DiegoAH291 <diegoaugustoh345@gmail.com>
2024-08-21 16:58:48 -0400

    feat: (all)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,14 +4,32 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "soft-slate-dark": "#1E1E28",
+        "lavender-gray": "#C8C8D0",
+        "soft-lavender-gray": "#ABAABC",
+        "soft-yellow": "#fefa41",
+        "soft-indigo": "#9747FF",
+      },
+
+      boxShadow: {
+        "indigo-soft":
+          "0 4px 6px -1px rgba(67, 56, 202, 0.1), 0 2px 4px -1px rgba(67, 56, 202, 0.06)",
+      },
+
+      animation: {
+        "fade-in": "fadeIn 1s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
       },
     },
   },
